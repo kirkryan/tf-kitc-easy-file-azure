f8fb2374b663cbea94fdb6ce3ffaa547a96eac43
Kirk Ryan <kirkryan@outlook.com>
2021-06-30 15:06:40 +0100

    remove in-line azure configuration - use env vars instead

--- main.tf
+++ main.tf
@@ -27,10 +27,6 @@ provider "netapp-cloudmanager" {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = var.azure_sub_id
-  client_id       = var.azure_client_id
-  client_secret   = var.azure_client_secret
-  tenant_id       = var.azure_tenant_id
 }
 
 variable "cloudmanager_refresh_token" {
