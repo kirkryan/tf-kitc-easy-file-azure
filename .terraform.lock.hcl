52390d4543dcb56820fdf3ec04569b4ce34db53b
Kirk Ryan <kirkryan@outlook.com>
2021-06-30 12:33:04 +0100

    here we go!

---
+++ .terraform.lock.hcl
@@ -0,0 +1,39 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/azurerm" {
+  version     = "2.46.0"
+  constraints = "2.46.0"
+  hashes = [
+    "h1:oprQ0h2inTISrOX35GtPDGr3chFSkqOkJlczqKJ2qsM=",
+    "zh:049938dab6f78a642a7c4147f99f146dc25824c738d0b18d448c665880d105fa",
+    "zh:139de4a46bbfc18b5403fa2264878a056fd8593d860942233913e54233c7327d",
+    "zh:19ceb7fc964265cb7e7f708e32b72a00089ed0398ae1f0014b5832078bd5be79",
+    "zh:6bf0b5ed0313188af6347354d8fe693abc708199eb732c19d876ae9cbef202ab",
+    "zh:ab2bb2f60e3daba204d3d8d47905b4815921a1455197bbd7530d71c604720a14",
+    "zh:c08b5cd280cb73504ace949f086db8420a7aee054833c6761d406829c18c6a15",
+    "zh:d5eccbc19d62c57c69d15c7f84b9ea3d83d1b78856fe4bc52b31dda8f91480cf",
+    "zh:e2b29b094a1ce46356dcc3e13693c0c651afc2a47d213d68ed6973b9fb40bae7",
+    "zh:f353830b47cced07d20dac1f2158962002f644a0240d3aa21fd5ec5e9e42c119",
+    "zh:f36ff78ae9de95f4216bb420ee4365cee7d70e95fac608fb650ae5aed5c04c1d",
+  ]
+}
+
+provider "registry.terraform.io/netapp/netapp-cloudmanager" {
+  version     = "21.1.1"
+  constraints = "21.1.1"
+  hashes = [
+    "h1:vnB4lLjbRhfqiulwMHwJW0ZN89BMtw8EeFkLO6pZ0n0=",
+    "zh:15b8ee7d6ca07377521746b5a446c8771bcba9c1f86dd53d6f55215b6800a742",
+    "zh:185ffe08042fcc0ffdb0f7def8d1042fce26c0e526f52d1be69d8bd437164c64",
+    "zh:1c17f4da4e32d776a4b0a658702f32a711e60042b9684849b39bd2f902122a9d",
+    "zh:37f713d32f3950f73f6c1670f698d73b9a44dffebf1715c8816a6ade61cd75cc",
+    "zh:48cc3fb2674f3e11044707ce8800c2f2a11a7058318321397d0ef318f1469bd4",
+    "zh:57fe4ddc0044ec5d4453521fb5220f6d0da56813fc983eac8aaf2225cff00f7f",
+    "zh:702f6175b087b3349098a032680fa80d60c5e94f27ec72f27258dae7d7d12138",
+    "zh:dcf444c17ad4e3e001f647b86a62c74c08f2d25da20c5a1ae7e3efc7ddbe78da",
+    "zh:e594a70db28b727586aae7a054f306dfd70b1be354a362dd2ff6378ef1a6285d",
+    "zh:ee5433d7ea90798f6085b6b8b7e9223cee5e31707c9a66969729d1d993105177",
+    "zh:fe67471821eb45c7d6ae0c02e31572bf3402eb6dad15f5914a34d17f52793925",
+  ]
+}
--- main.tf
+++ main.tf
@@ -4,10 +4,205 @@ terraform {
       source = "NetApp/netapp-cloudmanager"
       version = "21.1.1"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
   }
 }
 
 provider "netapp-cloudmanager" {
   # Configuration options
   refresh_token = var.cloudmanager_refresh_token
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = var.azure_sub_id
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+  tenant_id = var.azure_tenant_id
+}
+
+variable "cloudmanager_refresh_token" {
+  type = string
+  sensitive = true
+}
+
+variable "cloudmanager_location" {
+  type = string
+  default = "West Europe"
+}
+
+variable "azure_sub_id" {
+  type = string
+  sensitive = true
+}
+
+variable "cloudmanager_account_id" {
+  type = string
+  sensitive = true
+}
+
+variable "azure_client_id" {
+  type = string
+  sensitive = true
+}
+
+variable "azure_client_secret" {
+  type = string
+  sensitive = true
+}
+
+variable "azure_tenant_id" {
+  type = string
+  sensitive = true
+}
+
+resource "azurerm_resource_group" "rg_cloudmanager_connector" {
+  name     = "rg_cloudmanager_connector"
+  location = var.cloudmanager_location
+}
+
+resource "azurerm_virtual_network" "cloudmanager_network" {
+  name                = "cloudmanager_network"
+  location            = azurerm_resource_group.rg_cloudmanager_connector.location
+  resource_group_name = azurerm_resource_group.rg_cloudmanager_connector.name
+  address_space       = ["10.0.0.0/16"]
+
+  tags = {
+    environment = "kitc"
+  }
+}
+
+resource "azurerm_role_definition" "Azure_SetupAsService" {
+  name        = "AzureSetupAsService"
+  scope       = "/subscriptions/${var.azure_sub_id}"
+  description = "This is the role required to setup"
+
+  permissions {
+    actions     = [
+        "Microsoft.Compute/disks/delete",
+        "Microsoft.Compute/disks/read",
+        "Microsoft.Compute/disks/write",
+        "Microsoft.Compute/locations/operations/read",
+        "Microsoft.Compute/operations/read",
+        "Microsoft.Compute/virtualMachines/instanceView/read",
+        "Microsoft.Compute/virtualMachines/read",
+        "Microsoft.Compute/virtualMachines/write",
+        "Microsoft.Compute/virtualMachines/delete",
+        "Microsoft.Compute/virtualMachines/extensions/write",
+        "Microsoft.Compute/virtualMachines/extensions/read",
+        "Microsoft.Compute/availabilitySets/read",
+        "Microsoft.Network/locations/operationResults/read",
+        "Microsoft.Network/locations/operations/read",
+        "Microsoft.Network/networkInterfaces/join/action",
+        "Microsoft.Network/networkInterfaces/read",
+        "Microsoft.Network/networkInterfaces/write",
+        "Microsoft.Network/networkInterfaces/delete",
+        "Microsoft.Network/networkSecurityGroups/join/action",
+        "Microsoft.Network/networkSecurityGroups/read",
+        "Microsoft.Network/networkSecurityGroups/write",
+        "Microsoft.Network/virtualNetworks/checkIpAddressAvailability/read",
+        "Microsoft.Network/virtualNetworks/read",
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/read",
+        "Microsoft.Network/virtualNetworks/subnets/virtualMachines/read",
+        "Microsoft.Network/virtualNetworks/virtualMachines/read",
+        "Microsoft.Network/publicIPAddresses/write",
+        "Microsoft.Network/publicIPAddresses/read",
+        "Microsoft.Network/publicIPAddresses/delete",
+        "Microsoft.Network/networkSecurityGroups/securityRules/read",
+        "Microsoft.Network/networkSecurityGroups/securityRules/write",
+        "Microsoft.Network/networkSecurityGroups/securityRules/delete",
+        "Microsoft.Network/publicIPAddresses/join/action",
+        "Microsoft.Network/locations/virtualNetworkAvailableEndpointServices/read",
+        "Microsoft.Network/networkInterfaces/ipConfigurations/read",
+        "Microsoft.Resources/deployments/operations/read",
+        "Microsoft.Resources/deployments/read",
+        "Microsoft.Resources/deployments/delete",
+        "Microsoft.Resources/deployments/cancel/action",
+        "Microsoft.Resources/deployments/validate/action",
+        "Microsoft.Resources/resources/read",
+        "Microsoft.Resources/subscriptions/operationresults/read",
+        "Microsoft.Resources/subscriptions/resourceGroups/delete",
+        "Microsoft.Resources/subscriptions/resourceGroups/read",
+        "Microsoft.Resources/subscriptions/resourcegroups/resources/read",
+        "Microsoft.Resources/subscriptions/resourceGroups/write",
+        "Microsoft.Authorization/roleDefinitions/write",
+        "Microsoft.Authorization/roleAssignments/write",
+        "Microsoft.MarketplaceOrdering/offertypes/publishers/offers/plans/agreements/read",
+        "Microsoft.MarketplaceOrdering/offertypes/publishers/offers/plans/agreements/write",
+        "Microsoft.Network/networkSecurityGroups/delete",
+        "Microsoft.Storage/storageAccounts/delete",
+        "Microsoft.Storage/storageAccounts/write",
+        "Microsoft.Resources/deployments/write",
+        "Microsoft.Resources/deployments/operationStatuses/read",
+        "Microsoft.Authorization/roleAssignments/read"
+    ]
+    not_actions = []
+  }
+}
+
+resource "azurerm_network_security_group" "sg_cloudmanager" {
+  name                = "sg_cloudmanager"
+  location            = var.cloudmanager_location
+  resource_group_name = azurerm_resource_group.rg_cloudmanager_connector.name
+
+  security_rule {
+    name                       = "cloudmanager_connector"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_ranges          = ["22","80","443"]
+    destination_port_ranges     = ["*"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "cloudmanager_connector"
+    priority                   = 200
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_ranges          = ["*"]
+    destination_port_ranges     = ["*"]
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_subnet" "cloudmanager_subnet" {
+  name                 = "cloudmanager_subnet"
+  resource_group_name  = azurerm_resource_group.rg_cloudmanager_connector.name
+  virtual_network_name = azurerm_virtual_network.cloudmanager_network.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet_network_security_group_association" "cloudmanager_subsec_association" {
+  subnet_id                 = azurerm_subnet.cloudmanager_subnet.id
+  network_security_group_id = azurerm_network_security_group.sg_cloudmanager.id
+}
+
+resource "netapp-cloudmanager_connector_azure" "cl-occm-azure" {
+  provider = netapp-cloudmanager
+  name = "TF-ConnectorAzure"
+  location = var.cloudmanager_location
+  subscription_id = var.azure_sub_id
+  company = "kirkinthecloud"
+  resource_group =  azurerm_resource_group.rg_cloudmanager_connector.name
+  subnet_id = azurerm_subnet.cloudmanager_subnet.name
+  vnet_id = azurerm_virtual_network.cloudmanager_network.name
+  network_security_group_name = azurerm_network_security_group.sg_cloudmanager.name
+  associate_public_ip_address = true
+  account_id = var.cloudmanager_account_id
+  admin_password = "P@ssword123456"
+  admin_username = "vmadmin"
 }
